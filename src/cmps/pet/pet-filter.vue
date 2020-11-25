b751c715385cc51f46fe946cf1b269758b8b1755
RomanK <romanklebanov17@gmail.com>
2020-11-25 12:57:52 +0200

    filter

--- src/cmps/pet/pet-filter.vue
+++ src/cmps/pet/pet-filter.vue
@@ -1,11 +1,29 @@
 <template>
-<section class="pet-filter"></section>
+  <section class="pet-filter">
+    <h1>I am Filter</h1>
+    <input
+      type="text"
+      placeholder="Search for pets..."
+      v-model="filter.txt"
+      @input="debFilter"
+    />
+  </section>
 </template>
 
 <script>
+import mySelect from "./my-select";
+
 export default {
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      filter: {
+        txt: "",
+        type: "All",
+      },
+    };
+  },
+  components: {
+    mySelect,
+  },
+};
 </script>
