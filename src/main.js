d5bd565b571aa986e44cd5e60deedb868418646c
RomanK <romanklebanov17@gmail.com>
2020-11-25 13:46:46 +0200

    fyu

--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router.js'
 import store from './store/store.js'
+import { Button, Select } from 'element-ui';
 
 import '@/styles/main.scss';
 
 Vue.config.productionTip = false
-
+Vue.component(Select.name, Select);
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
