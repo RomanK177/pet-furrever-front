f2eccd512c2c45b60e28ef7dbf67ad5dca186263
Danielle Sibony <daniellesibony1@gmail.com>
2020-11-26 11:28:57 +0200

    pet-details

--- src/views/pet/pet-details.vue
+++ src/views/pet/pet-details.vue
@@ -1,17 +1,42 @@
 <template>
-<section class="pet-details"></section>
+  <section class="pet-details">
+    <h1>{{ pet.name }}</h1>
+    <img :src="pet.imgUrls" alt="" />
+    <p>{{ pet.description }}</p>
+    <p>{{ pet.age }}</p>
+    <p>{{ pet.gender }}</p>
+    <p>{{ pet.breed }}</p>
+    <p></p>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "petDetails",
   props: {
-    pet: Object
+    pet: Object,
   },
-  data(){
-    return{
-      
-    }
-  }
-}
+  data() {
+    return {
+      pet: {
+        name: "Winston",
+        gender: "male",
+        size: "medium",
+        type: "dog",
+        breed: "Australian Shepherd",
+        age: 1,
+        adoptedAt: null,
+        owner: {
+          _id: "s101",
+          name: "Dogs for life",
+          imgUrl: require("../assets/imgs/homepage/testimony1.jpg"),
+        },
+        description:
+          "Sweetest pup. He's incredibly smart. Loves to play frisbee, and run!",
+        imgUrls: [require("../assets/imgs/pets/aussie.jpg")],
+        tags: ["cute", "puppy", "friendly"],
+      },
+    };
+  },
+};
 </script>
