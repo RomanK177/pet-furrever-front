c9534302116b57f44f447569f843d83623b59bb3
Danielle Sibony <daniellesibony1@gmail.com>
2020-11-27 13:15:41 +0200

    scss

--- src/views/pet/pet-details.vue
+++ src/views/pet/pet-details.vue
@@ -16,30 +16,33 @@
           }"
         />
       </div>
-      <h1 class="pet-details-name">{{ pet.name }}</h1>
-      <p class="treats">Treat Count: {{ pet.numOfTreats }}</p>
-      <button class="adopt-btn">Adopt Me!</button>
-      <button class="treat-btn">Send Me a Treat!</button>
-      <p class="pet-bio">
-        A little About {{ pet.name }}: {{ pet.description }}
-      </p>
-      <p class="pet-details-age">Age: {{ pet.age }}</p>
-       <img
-        class="svg-symbol male"
-        v-if="pet.gender === 'male'"
-        src="../../assets/svgs/male-symbol.svg"
-        alt=""
-      />
-      <img
-        class="svg-symbol female"
-        v-if="pet.gender === 'female'"
-        src="../../assets/svgs/female-symbol.svg"
-        alt=""
-      />
-      <p class="pet-details-gender">Gender: {{ pet.gender }}</p>
-      <img src="../../assets/imgs/search.png" alt="" class="breed-icon">
-      <p class="pet-details-breed">Breed: {{ pet.breed }}</p>
-      <!-- <ul>
+      <div class="bio-adoption-container flex wrap">
+        <div class="pet-details-container">
+          <h1 class="pet-details-name">{{ pet.name }}</h1>
+          <hr>
+          <button class="btn treat-btn" >Send Me a Treat!</button>
+          <p class="treats">Treat Count: {{ pet.numOfTreats }}</p>
+          <h4 class="pet-about"> About:</h4>
+            <!-- A little About {{ pet.name }}: {{ pet.description }} -->
+          <p>{{pet.description}}</p>
+          <div class="pet-bio-container flex wrap space-between">
+          <p class="pet-details-age">Age: {{ pet.age }}</p>
+          <img
+            class="svg-symbol male"
+            v-if="pet.gender === 'male'"
+            src="../../assets/svgs/male-symbol.svg"
+            alt=""
+          />
+          <img
+            class="svg-symbol female"
+            v-if="pet.gender === 'female'"
+            src="../../assets/svgs/female-symbol.svg"
+            alt=""
+          />
+          <p class="pet-details-gender">Gender: {{ pet.gender }}</p>
+          <img src="../../assets/imgs/search.png" alt="" class="breed-icon" />
+          <p class="pet-details-breed">Breed: {{ pet.breed }}</p>
+          <!-- <ul>
       <li
         v-for="(tag, index) in pet.tags"
         :key="index"
@@ -48,19 +51,31 @@
         {{ tag }}
       </li>
       </ul> -->
-      <img :src="require(`@/assets/imgs/person/${pet.owner.imgUrl}`)" alt="" class="owner-img" />
-      <p class="pet-details-owner-name">Rescue: {{ pet.owner.name }}</p>
-      <h1 class="pet-details-comments-title">Comments</h1>
-      <hr />
-      <ul>
-        <li
-          v-for="(comment, index) in pet.comments"
-          :key="index"
-          class="pet-details-comments"
-        >
-          {{ comment.userName }}: {{ comment.comment }}
-        </li>
-      </ul>
+      </div>
+        </div>
+        <div class="petowner-details-container">
+          <img
+            :src="require(`@/assets/imgs/person/${pet.owner.imgUrl}`)"
+            alt=""
+            class="owner-img"
+          />
+          <p class="pet-details-owner-name">Rescue: {{ pet.owner.name }}</p>
+            <button class="btn adopt-btn">Adopt Me!</button>
+        </div>
+        <div class="comments-container">
+          <h1 class="pet-details-comments-title">Comments</h1>
+          <hr />
+          <ul>
+            <li
+              v-for="(comment, index) in pet.comments"
+              :key="index"
+              class="pet-details-comments"
+            >
+              {{ comment.userName }}: {{ comment.comment }}
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </section>
 </template>
