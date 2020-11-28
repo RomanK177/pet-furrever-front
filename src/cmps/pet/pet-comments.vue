c784b92ea058bed2161d391efeeadeb08545c532
Danielle Sibony <daniellesibony1@gmail.com>
2020-11-28 15:02:18 +0200

    added components

--- src/cmps/pet/pet-comments.vue
+++ src/cmps/pet/pet-comments.vue
@@ -1,11 +1,26 @@
 <template>
-<section class="pet-comments"></section>
+  <div class="comments-container">
+        <h1 class="pet-details-comments-title">Comments</h1>
+        <hr />
+        <ul>
+          <li
+            v-for="(comment, index) in pet.comments"
+            :key="index"
+            class="pet-details-comments"
+          >
+            {{ comment.userName }}: {{ comment.comment }}
+          </li>
+        </ul>
+        <input class="comment-input" type="text" placeholder="Add a Comment" />
+      </div>
 </template>
 
 <script>
 export default {
   props: {
-    msg: String
+    pet: {
+      type: Object
+    }
   }
 }
 </script>
