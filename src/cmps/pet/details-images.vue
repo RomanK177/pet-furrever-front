adf7bcdffedcff7c3552a9f60b8fe9f1f750d152
RomanK <romanklebanov17@gmail.com>
2020-11-28 17:14:47 +0200

    sdf

--- src/cmps/pet/details-images.vue
+++ src/cmps/pet/details-images.vue
@@ -1,26 +1,33 @@
 <template>
-      <div class="details-images">
-        <img
-          v-for="(imgUrl, index) in pet.imgUrls"
-          :key="index"
-          :src="require(`@/assets/imgs/pets/${imgUrl}`)"
-          alt=""
-          :class="{
-            item0: index === 0,
-            item1: index === 1,
-            item2: index === 2,
-            item3: index === 3,
-            item4: index === 4,
-          }"
-        />
-      </div>
+  <div class="details-images">
+    <img
+      v-for="(imgUrl, index) in pet.imgUrls"
+      :key="index"
+      :src="getUrl(imgUrl)"
+      alt=""
+      :class="{
+        item0: index === 0,
+        item1: index === 1,
+        item2: index === 2,
+        item3: index === 3,
+        item4: index === 4,
+      }"
+    />
+  </div>
 </template>
 <script>
 export default {
   name: "detailImages",
-   props: {
+  props: {
     pet: {
       type: Object,
+    },
+    methods: {
+      getUrl(url) {
+        let urlStart = url.slice(0, 4);
+        if (urlStart === "http") return url;
+        else return require(`../../assets/imgs/pets/${url}`);
+      },
     },
   },
 };
