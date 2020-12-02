f9114d23c481ca7c1e2944e0ec0d9edaae5ffc27
Danielle Sibony <daniellesibony1@gmail.com>
2020-12-02 22:43:45 +0200

    not sure

--- src/services/adoption-service.js
+++ src/services/adoption-service.js
@@ -13,15 +13,12 @@ function query() {
     return httpService.get(`adoptions`);
 }
 
-<<<<<<< HEAD
-function addAdoptionRequest(adoptionRequest) {
-    // debugger
-    return httpService.post(`adoptions`, adoptionRequest)
-=======
+// function addAdoptionRequest(adoptionRequest) {
+//     // debugger
+//     return httpService.post(`adoptions`, adoptionRequest)
 function addAdoptionRequest(petId) {
     debugger
     return httpService.post(`adoptions`, {petId});
->>>>>>> 6b6479dddac8f1344906ce6d3b728b8602b08c86
 }
 
 function updateAdoptionRequest(adoptionRequest) {
