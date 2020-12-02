c86086d5b265e102cbe0d20f7362a004594d1ccb
Danielle Sibony <daniellesibony1@gmail.com>
2020-12-02 14:30:54 +0200

    css

--- src/cmps/user/owner-details.vue
+++ src/cmps/user/owner-details.vue
@@ -4,22 +4,10 @@
       <router-link :to="'/user/edit/' + owner._id">Edit profile</router-link> ||
       <router-link to="/pet/edit">Add pet</router-link>
     </div>
-    <h1 v-if="checkIfOwner">Welcome {{ owner.fullName }}</h1>
+    <h1 v-if="checkIfOwner" class="welcome">Welcome {{ owner.fullName }}</h1>
     <h1 v-else>{{ owner.fullName }}</h1>
     <br />
-    <img class="user-profile-picture" :src="imgUrlProfile" />
-    <!-- :src="require(`@/assets/imgs/person/${pet.owner.imgUrl}`)" -->
-    <p><span class="bold">Name:</span> {{ owner.fullName }}</p>
-    <p><span class="bold">Email:</span> {{ owner.email }}</p>
-    <p><span class="bold">Telephone:</span> {{ owner.tel }}</p>
-    <p>
-      <span class="bold">Activity years:</span>
-      {{ owner.ownerData.activityYears }}
-    </p>
-    <p><span class="bold">Title:</span> {{ owner.ownerData.title }}</p>
-    <p><span class="bold">Description:</span> {{ owner.ownerData.desc }}</p>
-    <!-- Add tags from elemnts -->
-    <div class="details-images">
+        <div class="details-images">
       <img
         id="imgUploader2"
         v-for="(imgUrl, idx) in owner.ownerData.imgUrls"
@@ -33,6 +21,22 @@
           item4: idx === 4,
         }"
       />
+    </div>
+    <div class="owner-profile flex align-center">
+    <img class="user-profile-picture" :src="imgUrlProfile" />
+    <!-- :src="require(`@/assets/imgs/person/${pet.owner.imgUrl}`)" -->
+    <div class="owner-info">
+    <p><span class="bold">Name:</span> {{ owner.fullName }}</p>
+    <p><span class="bold">Email:</span> {{ owner.email }}</p>
+    <p><span class="bold">Telephone:</span> {{ owner.tel }}</p>
+    <p>
+      <span class="bold">Activity years:</span>
+      {{ owner.ownerData.activityYears }}
+    </p>
+    <p><span class="bold">Title:</span> {{ owner.ownerData.title }}</p>
+    <p><span class="bold">Description:</span> {{ owner.ownerData.desc }}</p>
+    <!-- Add tags from elemnts -->
+    </div>
     </div>
     <adoption-request
       @updateAdoption="updateAdoption"
