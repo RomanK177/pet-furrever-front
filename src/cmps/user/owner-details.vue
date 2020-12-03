eca027402027f66bca1562be3506d853cc10c562
RomanK <romanklebanov17@gmail.com>
2020-12-03 15:34:24 +0200

    dfg

--- src/cmps/user/owner-details.vue
+++ src/cmps/user/owner-details.vue
@@ -7,7 +7,7 @@
     <h1 v-if="checkIfOwner" class="welcome">Welcome {{ owner.fullName }}</h1>
     <h1 v-else>{{ owner.fullName }}</h1>
     <br />
-        <div class="details-images">
+    <div class="details-images">
       <img
         id="imgUploader2"
         v-for="(imgUrl, idx) in owner.ownerData.imgUrls"
@@ -23,26 +23,27 @@
       />
     </div>
     <div class="owner-profile flex align-center">
-    <img class="user-profile-picture" :src="imgUrlProfile" />
-    <!-- :src="require(`@/assets/imgs/person/${pet.owner.imgUrl}`)" -->
-    <div class="owner-info">
-    <p><span class="bold">Name:</span> {{ owner.fullName }}</p>
-    <p><span class="bold">Email:</span> {{ owner.email }}</p>
-    <p><span class="bold">Telephone:</span> {{ owner.tel }}</p>
-    <p>
-      <span class="bold">Activity years:</span>
-      {{ owner.ownerData.activityYears }}
-    </p>
-    <p><span class="bold">Title:</span> {{ owner.ownerData.title }}</p>
-    <p><span class="bold">Description:</span> {{ owner.ownerData.desc }}</p>
-    <!-- Add tags from elemnts -->
-    </div>
+      <img class="user-profile-picture" :src="imgUrlProfile" />
+      <!-- :src="require(`@/assets/imgs/person/${pet.owner.imgUrl}`)" -->
+      <div class="owner-info">
+        <p><span class="bold">Name:</span> {{ owner.fullName }}</p>
+        <p><span class="bold">Email:</span> {{ owner.email }}</p>
+        <p><span class="bold">Telephone:</span> {{ owner.tel }}</p>
+        <p>
+          <span class="bold">Activity years:</span>
+          {{ owner.ownerData.activityYears }}
+        </p>
+        <p><span class="bold">Title:</span> {{ owner.ownerData.title }}</p>
+        <p><span class="bold">Description:</span> {{ owner.ownerData.desc }}</p>
+        <!-- Add tags from elemnts -->
+      </div>
     </div>
     <adoption-request
       @updateAdoption="updateAdoption"
       @updatePet="updatePet"
       v-if="checkIfOwner"
       :requests="requests"
+      :user="owner"
     />
     <!-- <owner-review :owner="owner" /> -->
     <!-- <owner-review :owner="owner" :loggedInUser="loggedInUser" @addReview="updateReviews"/> -->
