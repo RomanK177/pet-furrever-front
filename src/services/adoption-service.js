81d844235185931ff9c77a937fc5b19899512e07
RomanK <romanklebanov17@gmail.com>
2021-02-16 20:58:43 +0200

    messages

--- src/services/adoption-service.js
+++ src/services/adoption-service.js
@@ -1,49 +1,42 @@
-import { httpService } from './http-service.js'
-
-
+import { httpService } from "./http-service.js";
 
 export const adoptionService = {
-    query,
-    addAdoptionRequest,
-    updateAdoptionRequest,
-    removeAdoptionRequest,
-    addMessage,
-    getAdoptionRequestById,
-    // markMessageAsUnread
-}
-
+  query,
+  addAdoptionRequest,
+  updateAdoptionRequest,
+  removeAdoptionRequest,
+  addMessage,
+  getAdoptionRequestById,
+  // markMessageAsUnread
+};
 
 function query() {
-    return httpService.get(`adoptions`);
+  return httpService.get(`adoptions`);
 }
 
 function addAdoptionRequest(petId) {
-    return httpService.post(`adoptions`, { petId });
+  return httpService.post(`adoptions`, { petId });
 }
 
 function getAdoptionRequestById(adoptionRequestId) {
-    return httpService.get(`adoptions/${adoptionRequestId}`);
+  return httpService.get(`adoptions/${adoptionRequestId}`);
 }
 
 function updateAdoptionRequest(adoptionRequest) {
-
-    return httpService.put(`adoptions/${adoptionRequest._id}`, adoptionRequest);
+  return httpService.put(`adoptions/${adoptionRequest._id}`, adoptionRequest);
 }
 
-
 function removeAdoptionRequest(adoptionRequestId) {
-    return httpService.delete(`adoptions/${adoptionRequestId}`);
+  return httpService.delete(`adoptions/${adoptionRequestId}`);
 }
 
 async function addMessage(adoptionId, message) {
-    return httpService.post(`adoptions/${adoptionId}/messages`, { message });
+  return httpService.post(`adoptions/${adoptionId}/messages`, { message });
 }
 
 // async function markMessageAsUnread(adoptionId) {
 //     return httpService.post(`adoptions/${adoptionId}/messages`)
 // }
-
-
 
 // function saveAdoption(adoptionRequest) {
 //     if (!adoptionRequest._id) {
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,121 +1,127 @@
-import { httpService } from './http-service.js';
+import { httpService } from "./http-service.js";
 
 export const userService = {
-    getEmptyUser,
-    signUp,
-    login,
-    logout,
-    getUsers,
-    getById,
-    remove,
-    update,
-    addReview,
-    updateFavorites
-}
+  getEmptyUser,
+  signUp,
+  login,
+  logout,
+  getUsers,
+  getById,
+  remove,
+  update,
+  addReview,
+  updateFavorites,
+};
 
 function getEmptyUser(type) {
-    return type === 'adopter' ? _getEmptyAdopter() : _getEmptyOwner();
+  return type === "adopter" ? _getEmptyAdopter() : _getEmptyOwner();
 }
 
 function _getCommonUserFields() {
-    return {
-        fullName: '',
-        userName: '',
-        password: '',
-        email: '',
-        tel: '0',
-        imgUrlProfile: '',
-        favoritePets:[]
-    }
+  return {
+    fullName: "",
+    userName: "",
+    password: "",
+    email: "",
+    tel: "0",
+    imgUrlProfile: "",
+    favoritePets: [],
+  };
 }
 
 function _getEmptyAdopter() {
-    var user = {
-        ..._getCommonUserFields(),
-        userType: 'adopter',
-        adopterData: {
-            ownPet: false,
-            ownedPet: false,
-            familyStatus: '',
-            houseStatus: ''
-        }
-    }
-    return user;
+  var user = {
+    ..._getCommonUserFields(),
+    userType: "adopter",
+    adopterData: {
+      ownPet: false,
+      ownedPet: false,
+      familyStatus: "",
+      houseStatus: "",
+    },
+  };
+  return user;
 }
 
 function _getEmptyOwner() {
-    var owner = {
-        ..._getCommonUserFields(),
-        userType: 'owner',
-        ownerData: {
-            activityYears: '0',
-            title: '',
-            desc: '',
-            tags: [],
-            imgUrls: [],
-            location: {
-                name: '',
-                lat: '',
-                lang: ''
-            },
-            reviews: [{
-                txt: '',
-                rate: 5,
-                by: {
-                    userId: '',
-                    fullName: '',
-                    imgUrl: ''
-                }
-            }]
-        }
-    }
-    return owner;
+  var owner = {
+    ..._getCommonUserFields(),
+    userType: "owner",
+    ownerData: {
+      activityYears: "0",
+      title: "",
+      desc: "",
+      tags: [],
+      imgUrls: [],
+      location: {
+        name: "",
+        lat: "",
+        lang: "",
+      },
+      reviews: [
+        {
+          txt: "",
+          rate: 5,
+          by: {
+            userId: "",
+            fullName: "",
+            imgUrl: "",
+          },
+        },
+      ],
+    },
+  };
+  return owner;
 }
 
 function getUsers() {
-    return httpService.get('users');
+  return httpService.get("users");
 }
 
 function getById(userId) {
-    return httpService.get(`users/${userId}`);
+  return httpService.get(`users/${userId}`);
 }
 
 function remove(userId) {
-    return httpService.delete(`users/${userId}`);
+  return httpService.delete(`users/${userId}`);
 }
 
 function update(user) {
-    console.log("🚀 ~ file: user-service.js ~ line 87 ~ update ~ user", user)
-    return httpService.put(`users/${user._id}`, user);
+  console.log("🚀 ~ file: user-service.js ~ line 87 ~ update ~ user", user);
+  return httpService.put(`users/${user._id}`, user);
 }
 
 async function login(userCred) {
-    const user = await httpService.post('auth/login', userCred)
-    if (!user) {
-        throw Error('login failed')
-    }
-    return _handleLogin(user)
+  console.log(
+    "🚀 ~ file: user-service.js ~ line 93 ~ login ~ userCred",
+    userCred
+  );
+  const user = await httpService.post("auth/login", userCred);
+  if (!user) {
+    throw Error("login failed");
+  }
+  return _handleLogin(user);
 }
 
 async function signUp(userCred) {
-    const user = await httpService.post('auth/signup', userCred);
-    return _handleLogin(user)
+  const user = await httpService.post("auth/signup", userCred);
+  return _handleLogin(user);
 }
 
 async function logout() {
-    await httpService.post('auth/logout');
-    sessionStorage.clear();
+  await httpService.post("auth/logout");
+  sessionStorage.clear();
 }
 
 async function addReview(ownerId, review) {
-    return httpService.post(`users/${ownerId}/reviews`, review);
+  return httpService.post(`users/${ownerId}/reviews`, review);
 }
 
 async function updateFavorites(isFavorite, petId) {
-    return httpService.post(`users/${petId}/favorites`, {isFavorite});
+  return httpService.post(`users/${petId}/favorites`, { isFavorite });
 }
 
 function _handleLogin(user) {
-    sessionStorage.setItem('user', JSON.stringify(user))
-    return user;
+  sessionStorage.setItem("user", JSON.stringify(user));
+  return user;
 }
